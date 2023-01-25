8319094483b32a83d88c4891193159d1939f28fa
Alejandro Aguilera <aguileraa18@gmail.com>
2023-01-25 10:30:09 -0600

    pages menu/configurations

---
+++ src/components/security/PermisosMenu.jsx
@@ -0,0 +1,44 @@
+
+export const OpcionesMenu = [
+    {
+        NombreLogico: 'MenuPrincipal',
+        NombreMenu: 'Menu Principal',
+        path:'/dashboard'
+    },
+    {
+        NombreLogico: 'Incapacidades',
+        NombreMenu: 'Incapacidades',
+        path:'/Incapacidades'
+    },
+    {
+        NombreLogico: 'DetalleIncapacidades',
+        NombreMenu: 'Detalle Incapacidades',
+        path:'/Detalle/Incapacidades'
+    },
+    {
+        NombreLogico: 'DetalleProducto',
+        NombreMenu: 'Detalle Producto',
+        path:'/Detalle/Productos'
+    },
+    {
+        NombreLogico: 'Kardex',
+        NombreMenu: 'Kardex',
+        path:'/Kardex'
+    },
+    {
+        NombreLogico: 'TrazabilidadKardex',
+        NombreMenu: 'Trazabilidad Kardex',
+        path:'/Kardex/Trazabilidad'
+    },
+    {
+        NombreLogico: 'RegistrosBase',
+        NombreMenu: 'Registros Base',
+        path:'/Registros/Base'
+    },
+    {
+        NombreLogico: 'CodigoBarras',
+        NombreMenu: 'Generar Codigo Barras',
+        path:'/barcode'
+    }
+    
+];
